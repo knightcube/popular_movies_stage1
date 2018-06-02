b76348f524288446686acac0adf8525af9869300
Rajat Kumar Gupta <rajatcube@gmail.com>
2018-06-02 06:45:22 +0530

    Update Constants.java

--- app/src/main/java/com/rajatcube/popularmovies/utils/Constants.java
+++ app/src/main/java/com/rajatcube/popularmovies/utils/Constants.java
@@ -6,7 +6,7 @@ package com.rajatcube.popularmovies.utils;
 
 public class Constants {
 
-    public static final String API_KEY = "38f11004ca2a26cd99baf97eb11c9c7b";
+    //public static final String API_KEY = "YOUR_API_KEY_HERE";
     public static final String BASE_URL = "https://api.themoviedb.org/3/movie";
     public static final String POPULAR_MOVIES_BASE_URL = BASE_URL+"/popular?api_key="+API_KEY;
     public static final String TOP_RATED_MOVIES_BASE_URL = BASE_URL+"/top_rated?api_key="+API_KEY;
